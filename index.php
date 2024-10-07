b1f9c97b5a6ecc7303ebfc313e180ec31603d9eb
aerdripz <dimasantariksa155@gmail.com>
2024-10-08 00:36:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Bunny Sayang Aku Nggak?</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="banner" id="myBanner">
+    <h1>Bunny Sayang Aku Nggak?</h1>
+    <button id="yesButton">Iya</button>
+    <button id="noButton">Tidak</button>
+  </div>
+  <div id="message" style="display: none;">
+    <h1>Makasih Sayang!</h1>
+  </div>
+  <script src="script.js"></script>
+</body>
+</html>
